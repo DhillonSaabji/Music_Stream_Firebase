020eaafd5b037520af60b08186f326fb034fa604
DhillonSaabji <blackdaimondprogramer@gmail.com>
2020-12-25 16:42:28 +0530

    netflix

---
+++ fluterproject/beatum/demo/Netflix/stream_plus2/lib/style/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class Colors {
+
+  const Colors();
+
+  static const Color mainColor = const Color(0xFF151C26);//0xFF42A5F5
+  static const Color secondColor = const Color(0x808000);//.fromARGB(255, 66, 165, 245);
+  static const Color titleColor = const Color(0xFF5a606b);
+
+}
